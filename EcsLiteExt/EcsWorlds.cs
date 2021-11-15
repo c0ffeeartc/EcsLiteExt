02df055c5c417914fa1c85d2550c3370d519bd5d
c0ffeeartc <c0ffeeartc@gmail.com>
2021-11-15 09:41:26 +0200

    + add EcsWorlds.cs

---
+++ EcsLiteExt/EcsWorlds.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using Leopotam.EcsLite;
+
+namespace EcsLite.Custom
+{
+public sealed class EcsWorlds
+{
+	public static EcsWorlds I = new EcsWorlds();
+
+	private List<EcsWorld> _worlds = new List<EcsWorld>();
+	public int Count => _worlds.Count;
+	public List<EcsWorld> Worlds => _worlds;
+
+	public EcsWorld Add (EcsWorld ecsWorld, out int worldId)
+	{
+		worldId = _worlds.Count;
+		_worlds.Add(ecsWorld);
+		return ecsWorld;
+	}
+
+	public EcsWorld Get(Int32 worldId)
+	{
+		return _worlds[worldId];
+	}
+}
+}
